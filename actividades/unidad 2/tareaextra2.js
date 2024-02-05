3b44d82d52d3645bf4efaae938a3beeedd599b0d
Y-Sophia-L <158627522+Y-Sophia-L@users.noreply.github.com>
2024-02-05 15:06:59 -0600

    Add files via upload

---
+++ actividades/unidad 2/tareaextra2.js
@@ -0,0 +1,28 @@
+var usuario
+return Math.floor(Math.random() * (11 - 1) + 1;
+         return  Math.floor(Math.random() * (11 - 1) + 1;
+var oponente
+var ran = 0;
+
+while(contador < 21){
+  contador = prompt(¿Quieres otra carta?)
+  uruario = Math.floor(Math.random() * (11 - 1) + 1;
+                }       
+   if(usuario > 21){        
+  alert("perdio")
+  
+  usuario > oponente
+  }
+  while(contador < 21){
+  contador = prompt(¿Quieres otra carta?)
+  uruario = Math.floor(Math.random() * (11 - 1) + 1;
+                }     
+                     if(usuario < 21){        
+  alert("puedes agarrar otra carta")
+  } 
+  if(usuario < 21){        
+  alert("el jugador tiene.....")
+  } 
+    if(usuario < 21){        
+  alert("puedes agarrar otra carta")
+  } 
